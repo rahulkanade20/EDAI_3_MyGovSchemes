b6dc262469c37c315da1fecaa31b318ddde5cabd
rahulkanade20 <krahul.view@gmail.com>
2020-09-19 20:51:52 +0530

    database added and java classes made

---
+++ app/src/main/java/com/example/myscheme10/Family.java
@@ -0,0 +1,26 @@
+package com.example.myscheme10;
+
+import java.util.List;
+
+public class Family {
+
+    private String surname;
+    private int noOfMembers;
+    private String state;
+    private String city;
+    private String caste;
+    private int familyIncome;
+
+    List<Member> members;
+
+    public boolean addMember() {
+
+        return true;
+    }
+
+    public boolean save() {
+
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/example/myscheme10/Member.java
@@ -0,0 +1,25 @@
+package com.example.myscheme10;
+
+import java.util.List;
+
+public class Member {
+
+    private String name;
+    private int age;
+    private String occupation;
+    private int yearlyIncome;
+    private String gender;
+
+    public boolean add() {
+
+        return true;
+    }
+
+    public List<String> getRecommendations() {
+
+        List<String> listName = null;
+
+        return listName;
+    }
+
+}
